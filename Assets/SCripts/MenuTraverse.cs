60aa8a47ad28fa08138628784c8b52046e11b509
Galinath <nathangalicia360@gmail.com>
2025-05-22 13:14:43 -0700

    new

--- Assets/SCripts/MenuTraverse.cs
+++ Assets/SCripts/MenuTraverse.cs
@@ -1,15 +1,42 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
+using UnityEngine.UI; // For legacy Text component
+
 public class MainTraverse : MonoBehaviour
 {
+    
+    [SerializeField] private Text titleText;
+    
+    [SerializeField] private AudioSource audioSource;
+
     public void Play()
     {
-        SceneManager.LoadScene("Game");
+      
+        StartCoroutine(PlayWithTitleChangeAndAudio());
     }
- 
+
+    private IEnumerator PlayWithTitleChangeAndAudio()
+    {
+        
+        if (titleText != null)
+        {
+            titleText.text = "<color=red>Perish</color> of Man";
+        }
+        
+
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+       
+       
+        yield return new WaitForSeconds(6.5f);
+
+        
+        SceneManager.LoadScene("Level 1");
+    }
+
     public void Quit()
     {
         Application.Quit();

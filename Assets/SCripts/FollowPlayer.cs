296e5771ff422a59db8ac3ece32038b6bcac30e7
Galinath <nathangalicia360@gmail.com>
2025-05-27 13:30:15 -0700

    boom

---
+++ Assets/SCripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float followSpeed = 5f;
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = player.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}

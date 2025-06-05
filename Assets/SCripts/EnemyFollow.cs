f03b397939c64ba8db9dd87c85a8645c2165d743
RaphaelQ116 <raphaelquinto116@gmail.com>
2025-06-05 13:19:04 -0700

    q

--- Assets/SCripts/EnemyFollow.cs
+++ Assets/SCripts/EnemyFollow.cs
@@ -20,7 +20,7 @@ public class EnemyFollow : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if(distance < 4)
+        if(distance < 1000)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
